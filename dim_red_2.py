7721c4c8d444c6d05d416d1c36768c9f7384300f
manic-mailman <derrekkchu@gmail.com>
2016-02-05 16:44:05 -0800

    with better stopwords and log(term frequency)

--- dim_red_2.py
+++ dim_red_2.py
@@ -1,10 +1,11 @@
-#combines vectorization via tf-idf and svd into one script
+#combines vectorization + svd into one script
 
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
 from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
 
 #reading in the crap, minor preprocessing
 responses = []
@@ -17,26 +18,54 @@ with open('data') as f:
 			line = ' '.join([lemmatizer.lemmatize(word.lower()) for word in words])
 			responses.append(line)
 
-#vectorizing, removing all tokens that either show up only once, or on more than 10% of the the hero responses 
-vectorizer = TfidfVectorizer(stop_words = 'english', max_df = 0.1, min_df = 2)
+#vectorizing, removing all tokens that either show up only once, or on more than 25% of the the hero responses 
+#modifying the stoplist with the frequent terms that still show up despite the thresholding
+stopwords = set(list(ENGLISH_STOP_WORDS) + 
+	['aha', 
+	'wont', 
+	'hee', 
+	'hoo', 
+	'hu', 
+	'heah', 
+	'ya', 
+	'hh', 
+	'ti', 
+	'aw', 
+	'haha', 
+	'em', 
+	'said', 
+	'hey', 
+	'id', 
+	'le', 
+	'hoh', 
+	'heheh',
+	'gonna',
+	'dy',
+	'agh'
+	'stay'])
+vectorizer = TfidfVectorizer(stop_words = stopwords, max_df = 0.25, min_df = 2, sublinear_tf = True)
 tfidf_mat = vectorizer.fit_transform(responses)
 
 #svd
-svd = TruncatedSVD(n_components = 300, random_state = 1)
+svd = TruncatedSVD(n_components = 3, random_state = 1)
 dim_red_mat = svd.fit_transform(tfidf_mat)
-print(sum(svd.explained_variance_ratio_))
+print(np.cumsum(svd.explained_variance_ratio_))
 
 #getting the top words for each componenet--might not be very meaningful
 def get_component_words(n_words, n_components, fitted_model, feature_names):
 	'''a function to extract the top n_words from the top n_componenets of fitted_model, irregardless of sign. 
 	requires a correspondng list of feature_names.  
-	returns a list of tuples of (coefficient, feature).'''
+	returns a list of tuples of (coefficient, feature) for each component.'''
 
 	top_features = []
 	for component in fitted_model.components_[0:n_components]:
 		top_features.append([(component[i], feature_names[i]) for i in np.absolute(component).argsort()[::-1][0:n_words]])
 	return(top_features)
-print(get_component_words(n_words = 5, fitted_model = svd, n_components = 3, feature_names = vectorizer.get_feature_names()))
+
+top_10_words = get_component_words(n_words = 10, fitted_model = svd, n_components = 3, feature_names = vectorizer.get_feature_names())
+for words in top_10_words:
+	print(words)
+	print('\n')
 
 #plotting
 hero_names = [line.split(' ')[0] for line in responses]
